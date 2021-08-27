b6a5936e37767a55ce7d6e15c65dfd060b63332e
BuildTools <kaffarell>
2021-08-27 23:47:36 +0200

    test: added jest unit test for board

--- src/main.js
+++ src/main.js
@@ -1,16 +1,27 @@
 require('dotenv').config();
 const axios = require('axios');
 
-console.log(process.env.key);
-console.log(process.env.token);
 
 // 4BT board id:
-let boardId = '5f622509e65281827b2e2e59'
+// let boardId = '5f622509e65281827b2e2e59'
 
-axios.get(`https://api.trello.com/1/members/me/boards?key=${process.env.key}&token=${process.env.token}`)
-    .then(response => {
-        console.log(response.data.map);
-    })
-    .catch(error => {
-        console.log(error);
+function getBoard() {
+    return new Promise((resolve, reject) => {
+        axios.get(`https://api.trello.com/1/members/me/boards?key=${process.env.key}&token=${process.env.token}`)
+            .then(response => {
+                let board = response.data.find(object => object.name === '4BT');
+                resolve(board);
+            })
+            .catch(error => {
+                console.log(error);
+            });
     });
+}
+
+( async () => {
+
+    //console.log( await getBoard());
+
+})();
+
+module.exports = getBoard;
